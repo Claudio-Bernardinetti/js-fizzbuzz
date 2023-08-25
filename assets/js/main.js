c64cfe1c0668484384a052d6e992663cde40996a
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-08-25 15:41:22 +0100

    add output

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,42 @@
-/* Consegna:
-Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+// Scriver un for loop per stampare i numeri da 1 a 100 in console.
 
- per i multipli di 3 stampi “Fizz” al posto del numero
-e per i multipli di 5 stampi “Buzz”.
+// Per i multipli di 3 stampi “Fizz” al posto del numero.
 
-Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Per i multipli di 5 stampi “Bizz” al posto del numero.
 
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Come faccio a sapere se un numero è divisibile per un altro?
-Abbiamo visto qualcosa di particolare che possiamo usare (si)?
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-Consigli del giorno:
-Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 
-BONUS 1:
-Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 
-BONUS 2:
-Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
 
-console.log('ciao');
+// let listEl = document.querySelector('.list');
+
+
+// for (var i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+    
+    
+
+//     const listIt = document.createElement('li');
+//     listIt.append(i);
+//     listEl.append(listIt);
+// }
+
+function fizz (num){
+    for (let i = 1; i < num; i++){
+      let element = document.createElement('li')
+      element.innerText = "";
+      if(i % 3 === 0 ) {element.innerText += "Fizz";}
+      if(i % 5 === 0 ) {element.innerText += "Buzz";}
+      if(element.innerText === ""){element.innerText = i;}
+      document.getElementById('output').appendChild(element);
+    }
+ }
+ 
+ fizz(101);
