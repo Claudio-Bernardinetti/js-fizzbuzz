b7a47f856ef6e71a7d8703644d0771db6c5dd18d
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-08-25 18:02:08 +0100

    add color fizz bazz

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,26 @@
 // Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 
 
-// let listEl = document.querySelector('.list');
-
-
-// for (var i = 1; i < 101; i++) {
-//     if (i % 15 == 0) console.log("FizzBuzz");
-
-//     else if (i % 3 == 0) console.log("Fizz");
-//     else if (i % 5 == 0) console.log("Buzz");
-//     else console.log(i);
-    
-    
-
-//     const listIt = document.createElement('li');
-//     listIt.append(i);
-//     listEl.append(listIt);
-// }
-
 function fizz (num){
     for (let i = 1; i < num; i++){
       let element = document.createElement('li')
-      element.innerText = "";
-      if(i % 3 === 0 ) {element.innerText += "Fizz";}
-      if(i % 5 === 0 ) {element.innerText += "Buzz";}
-      if(element.innerText === ""){element.innerText = i;}
+      element.innerText = i;
+      
+      if (i % 3 === 0 && i % 5 === 0 ) {
+        element.innerText = "FizzBuzz";
+        element.classList.add('red');
+
+      } else if (i % 3 === 0 ) {
+        element.innerText = "Fizz";
+        element.classList.add('purple');
+      
+      } else if(i % 5 === 0 ) {
+        element.innerText = "Buzz";
+        element.classList.add('yellow');
+      }
+
       document.getElementById('output').appendChild(element);
+  
     }
  }
  
